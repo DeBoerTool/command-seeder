ce3884a0d6b32ccfffd7c8128bba6d1399cfcb54
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2023-04-30 09:16:17 -0400

    Initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Dbt\LaravelProject;
+
+use Illuminate\Support\ServiceProvider as LaravelProvider;
+
+class ServiceProvider extends LaravelProvider
+{
+    public function register (): void
+    {
+    }
+
+    public function boot (): void
+    {
+    }
+}
